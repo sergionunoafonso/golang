35b2cc40746b5aab9ad14412b0aed8d2f7cd66fb
Sérgio Nuno Afonso <sergionunoafonso@gmail.com>
2018-04-07 16:07:31 +0100

    Change hello.go to use stringutil

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sergionunoafonso/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
